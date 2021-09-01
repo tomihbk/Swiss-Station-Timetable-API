21270cda7c100a46a2bf9aa41b7cc1c44d9586b2
Thomas Berhane <thomasnegassi@gmail.com>
2021-09-01 22:27:09 +0200

    feat: add CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix(process.env.API_VERSION);
   await app.listen(process.env.APP_PORT);
 }
