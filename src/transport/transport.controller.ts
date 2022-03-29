68394439b59ab14efd2c2421d5bfc02c0d325336
Thomas <thomasnegassi@gmail.com>
2022-03-30 00:01:13 +0200

    fix: ts type added to transport

--- src/transport/transport.controller.ts
+++ src/transport/transport.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common
 import { TransportBodyRequestDto } from './dto/transport.dto';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { TransportService } from './transport.service';
+import { AxiosResponse } from 'axios';
 
 @Controller('transport')
 export class TransportController {
@@ -9,7 +10,7 @@ export class TransportController {
 
     @Post()
     @UsePipes(ValidationPipe)
-    getTransportData(@Body() transportBodyRequestDto: TransportBodyRequestDto): any {
+    getTransportData(@Body() transportBodyRequestDto: TransportBodyRequestDto): Promise<AxiosResponse<any>> {
         return this.TransportService.getTransportData(transportBodyRequestDto)
     }
 }
