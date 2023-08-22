46f172d26f06c13e1856000bbd877a09bd29e7d9
Thomas <thomasnegassi@gmail.com>
2023-08-23 00:08:36 +0200

    fix: delete unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino'
 import { TransportController } from './transport/transport.controller';
-import { Transport } from './transport';
 import { TransportService } from './transport/transport.service';
 import { ConfigModule } from '@nestjs/config';
 import { TransportModule } from './transport/transport.module';
 import { StationsController } from './stations/stations.controller';
 import { StationsModule } from './stations/stations.module';
+import { StationsService } from './stations/stations.service';
 //process.env.NODE_ENV === 'development' ? 
 @Module({
   imports: [LoggerModule.forRoot({
@@ -20,7 +20,7 @@ import { StationsModule } from './stations/stations.module';
     }
   }), ConfigModule.forRoot(), TransportModule, StationsModule],
   controllers: [AppController, TransportController, StationsController],
-  providers: [AppService, Transport, TransportService],
+  providers: [AppService, TransportService, StationsService],
 })
 
 export class AppModule { }
